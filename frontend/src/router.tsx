d32d2a20f19f19515b4285adb8f8086f2181ba7f
Marcus Elcamel <conti.marcus6@gmail.com>
2025-05-14 19:51:43 +0100

    STYLE: finalize homepage layout and wallpaper integration

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,11 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import CharacterPage from './components/pages/CharacterPage';
-import HomePage from './components/pages/HomePage';
-import ProfilePage from './components/pages/ProfilePage/ProfilePage.tsx';
+import ProfilePage from './components/pages/ProfilePage.tsx';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/me', element: <ProfilePage /> },
+  { path: '/', element: <ProfilePage /> },
   { path: '/game', element: <CharacterPage /> },
 ]);
 
